2545b7bbc14e3b1f9e1cee4aab41d479dad031ad
Aru <strangebear@outlook.com>
2026-06-04 21:44:17 +0800

    构建fastapi基本框架

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 class Settings(BaseSettings):
@@ -8,7 +11,7 @@ class Settings(BaseSettings):
     """
 
     model_config = SettingsConfigDict(
-        env_file='.env',
+        env_file=str(BASE_DIR / '.env'),
         env_file_encoding='utf-8',
         extra='ignore',
     )
